0f9551083d3175a9f16e16d4f6d9874af5174706
Eric Cho <yeonsikcho@gmail.com>
2021-06-11 15:53:53 -0400

    first edit

---
+++ SOF586/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+import datetime
+import json
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
