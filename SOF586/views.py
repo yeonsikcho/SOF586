13faa8b9f7eb591c45eef41399dab5f3676ff295
Eric Cho <yeonsikcho@gmail.com>
2021-09-09 16:29:37 -0400

    save

--- SOF586/views.py
+++ SOF586/views.py
@@ -22,7 +22,7 @@ def menu(request):
     # con = sql.connect(host = 'analytiq.co.kr', user = 'sof586', database = 'sof586', charset = 'utf8')
     cursor = con.cursor()
     #Get menu prices
-    cursor.execute("select * from menu_items")
+    cursor.execute("select * from sof586.menu_items")
     prices = {c[1]:int(c[2]) for c in cursor.fetchall()}
     con.close()
     return render(request, 'menu.html', prices)
@@ -34,25 +34,25 @@ def place_order(request):
     # con = sql.connect(host = 'analytiq.co.kr', user = 'sof586', database = 'sof586', charset = 'utf8')
     cursor = con.cursor()
     #Get menu item to id map
-    cursor.execute("select menu_name, id from menu_items")
+    cursor.execute("select menu_name, id from sof586.menu_items")
     idmap = {c[0]:c[1] for c in cursor.fetchall()}
     #Get Customer ID
-    nres = cursor.execute("select id from customer where name = %s", [customer['name']])
+    nres = cursor.execute("select id from sof586.customer where name = %s", [customer['name']])
     if nres == 0:
         #If customer is not found, add Customer Information
-        cursor.execute("select max(id)+1 from customer")
+        cursor.execute("select max(id)+1 from sof586.customer")
         customer_id = cursor.fetchone()[0]
-        cursor.execute("insert into customer values (%s,%s,%s,%s)",[customer_id, customer['name'], customer['address'], customer['phone']])
+        cursor.execute("insert into sof586.customer values (%s,%s,%s,%s)",[customer_id, customer['name'], customer['address'], customer['phone']])
     else: #If customer is found, update address and phone
         #Update Address Information
         customer_id = cursor.fetchone()[0]
-        cursor.execute("update customer set address = %s, phone_no = %s where id = %s", [customer['name'], customer['phone'], customer_id])
+        cursor.execute("update sof586.customer set address = %s, phone_no = %s where id = %s", [customer['name'], customer['phone'], customer_id])
     #Insert orders into orders table
-    cursor.execute("select max(id)+1 from orders")
+    cursor.execute("select max(id)+1 from sof586.orders")
     order_id = cursor.fetchone()[0]
-    cursor.execute("insert into orders values (%s,%s,%s)",[order_id, customer_id, datetime.datetime.now()])
+    cursor.execute("insert into sof586.orders values (%s,%s,%s)",[order_id, customer_id, datetime.datetime.now()])
     for item in cart:
-        cursor.execute("insert into order_items values (%s,%s,%s)",[order_id, idmap[item], cart[item]])
+        cursor.execute("insert into sof586.order_items values (%s,%s,%s)",[order_id, idmap[item], cart[item]])
     con.commit()
     con.close()
     return JsonResponse({'msg':'Order submited successfully'})
@@ -61,10 +61,10 @@ def shopper_history_report(request):
     con = psycopg2.connect(host = 'hostinger.eric-cho.com', database = 'postgres', user = 'postgres', password = '0501cho', port = '5432')
     # con = sql.connect(host = 'analytiq.co.kr', user = 'sof586', database = 'sof586', charset = 'utf8')
     cursor = con.cursor()
-    cursor.execute("""select name, order_id, order_time, menu_name, quantity from orders 
-    inner join customer on orders.customer_id = customer.id 
-    inner join order_items on orders.id = order_items.order_id
-    inner join menu_items on order_items.item_id = menu_items.id""")
+    cursor.execute("""select name, order_id, order_time, menu_name, quantity from sof586.orders 
+    inner join sof586.customer on orders.customer_id = customer.id 
+    inner join sof586.order_items on orders.id = order_items.order_id
+    inner join sof586.menu_items on order_items.item_id = menu_items.id""")
     history = cursor.fetchall()
     con.close()
     history_dic = {}
--- update_menu.py
+++ update_menu.py
@@ -6,6 +6,6 @@ con = psycopg2.connect(host = 'hostinger.eric-cho.com', database = 'postgres', u
 # con = sql.connect(host = 'analytiq.co.kr', user = 'sof586', database = 'sof586', charset = 'utf8')
 cursor = con.cursor()
 for i,d in df.iterrows():
-    cursor.execute("insert into menu_items values (%s,%s,%s) on duplicate key update menu_name = %s, price = %s"
+    cursor.execute("insert into sof586.menu_items values (%s,%s,%s) on duplicate key update menu_name = %s, price = %s"
     ,[d['id'], d['menu'], d['price'], d['menu'], d['price']])
 con.commit()
